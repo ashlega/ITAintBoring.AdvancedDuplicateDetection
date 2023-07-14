7d0b1563888f15c8ee66d5ea653c17d77b8e8405
ashlega <ashlega@yahoo.com>
2023-07-13 23:10:56 -0400

    Initial Commit

---
+++ Plugins/Admin/Actions.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using System.Linq;
+
+namespace ITAintBoring.AdvancedDuplicateDetection.Admin
+{
+
+    public class Actions : IPlugin
+    {
+
+        public Actions(string unsecureString, string secureString)
+        {
+        }
+
+        /*
+         * stepName - how to name the step
+         * ita_sdkmessagename - sdkmessagename
+         * ita_attributes - attributes
+         * description - step description
+         * configuration - step configuration
+         * stepId - stepid for existing step (for update/delete)
+         * action - CreateStep/UpdateStep/DeleteStep
+         * ita_tablename - table/enttiy name
+         * 
+         */
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            string action = (string)context.InputParameters["ita_action"];
+            Guid sdkMessageProcessingStepId = context.InputParameters.Contains("ita_stepid") ? (Guid)context.InputParameters["ita_stepid"] : Guid.Empty;
+
+            if (action == "DeleteStep")
+            {
+                service.Delete("sdkmessageprocessingstep", sdkMessageProcessingStepId);
+            }
+            else
+            {
+
+                string stepName = context.InputParameters.Contains("ita_stepname") ? (string)context.InputParameters["ita_stepname"] : null;
+                string stepAttributes = context.InputParameters.Contains("ita_attributes") ? (string)context.InputParameters["ita_attributes"] : null;
+                string sdkMessageName = context.InputParameters.Contains("ita_sdkmessagename") ? (string)context.InputParameters["ita_sdkmessagename"] : "Create";
+                string tableName = context.InputParameters.Contains("ita_tablename") ? (string)context.InputParameters["ita_tablename"] : null;
+                string description = context.InputParameters.Contains("ita_description") ? (string)context.InputParameters["ita_description"] : null;
+                string configuration = context.InputParameters.Contains("ita_configuration") ? (string)context.InputParameters["ita_configuration"] : null;
+                OptionSetValue mode = new OptionSetValue(0);
+                int rank = 1;
+                OptionSetValue stage = new OptionSetValue(10);
+                OptionSetValue supportedDeployment = new OptionSetValue(0);
+                OptionSetValue invocationSource = new OptionSetValue(0);
+                
+
+                //Get plugin type id for DuplicateDetection
+                QueryExpression qe = new QueryExpression("plugintype");
+                qe.Criteria.AddCondition("name", ConditionOperator.Equal, "ITAintBoring.AdvancedDuplicateDetection.BusinessLogic.DuplicateDetection");
+                qe.ColumnSet = new ColumnSet("plugintypeid");
+                var pluginTypeResult = service.RetrieveMultiple(qe).Entities.FirstOrDefault();
+
+                EntityReference pluginTypeId = pluginTypeResult != null ? pluginTypeResult.ToEntityReference() : null;
+
+
+                //Get message id for Create
+                qe = new QueryExpression("sdkmessage");
+                qe.Criteria.AddCondition("name", ConditionOperator.Equal, sdkMessageName);
+                qe.ColumnSet = new ColumnSet("sdkmessageid");
+                var sdkMessageResult = service.RetrieveMultiple(qe).Entities.FirstOrDefault();
+
+
+
+                EntityReference sdkMessageId = sdkMessageResult != null ? sdkMessageResult.ToEntityReference() : null;
+
+
+                EntityReference sdkMessageFilterId = null;
+
+                if (sdkMessageId != null && action != "UpdateStep")
+                {
+                    //Get message id for Create
+                    qe = new QueryExpression("sdkmessagefilter");
+                    qe.Criteria.AddCondition("primaryobjecttypecode", ConditionOperator.Equal, tableName);
+                    qe.Criteria.AddCondition("sdkmessageid", ConditionOperator.Equal, sdkMessageId.Id);
+                    qe.ColumnSet = new ColumnSet("sdkmessagefilterid");
+                    var sdkMessageFilterResult = service.RetrieveMultiple(qe).Entities.FirstOrDefault();
+                    sdkMessageFilterId = sdkMessageFilterResult != null ? sdkMessageFilterResult.ToEntityReference() : null;
+                }
+
+
+                //throw new InvalidPluginExecutionException((string)sdkMessageFilterResult["primaryobjecttypecode"]);
+
+                if (action == "CreateStep")
+                {
+                    Entity step = new Entity("sdkmessageprocessingstep");
+                    step["name"] = stepName;
+                    step["description"] = description;
+                    step["configuration"] = configuration;
+                    step["mode"] = mode;
+                    step["rank"] = rank;
+                    step["stage"] = stage;
+                    step["supporteddeployment"] = supportedDeployment;
+                    step["invocationsource"] = invocationSource;
+                    step["plugintypeid"] = pluginTypeId;
+                    step["sdkmessageid"] = sdkMessageId;
+                    step["filteringattributes"] = stepAttributes;
+                    step["sdkmessagefilterid"] = sdkMessageFilterId;
+                    step.Id = service.Create(step);
+                }
+                else if (action == "UpdateStep")
+                {
+                    Entity step = new Entity("sdkmessageprocessingstep");
+                    //step["name"] = stepName;
+                    step["description"] = description;
+                    step["configuration"] = configuration;
+                    step["filteringattributes"] = stepAttributes;
+                    //step["mode"] = mode;
+                    //step["rank"] = rank;
+                    //step["stage"] = stage;
+                    //step["supporteddeployment"] = supportedDeployment;
+                    //step["invocationsource"] = invocationSource;
+                    //step["plugintypeid"] = pluginTypeId;
+                    //step["sdkmessageid"] = sdkMessageId;
+                    //step["sdkmessagefilterid"] = sdkMessageFilterId;
+                    step.Id = sdkMessageProcessingStepId;
+                    service.Update(step);
+                }
+            }
+  
+
+        }
+    }
+}
---
+++ Plugins/DuplicateDetection/DuplicateDetection.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace ITAintBoring.AdvancedDuplicateDetection.BusinessLogic
+{
+    [DataContract]
+    public class PluginConfig
+    {
+        [DataMember] public string FetchXML;
+        [DataMember] public string ErrorMessage;
+        [DataMember] public bool ErrorWhenEmpty;
+
+    }
+
+    public class DuplicateDetection : IPlugin
+    {
+
+
+        PluginConfig config = null;
+
+
+        public DuplicateDetection(string unsecureString, string secureString)
+        {
+            if (unsecureString == null) return;
+            
+            string configJson = unsecureString.Replace('\r', ' ').Replace('\n', ' ');
+            var deserializedConfig = new PluginConfig();
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(configJson));
+            var ser = new DataContractJsonSerializer(deserializedConfig.GetType());
+            config = ser.ReadObject(ms) as PluginConfig;
+            ms.Close();
+        }
+
+
+        public static string getStringValue(Entity record, string key)
+
+        {
+
+            string result = "";
+            object value = null;
+            
+            if (!record.Contains(key)) return result;
+
+            if (record[key] is AliasedValue)
+            {
+                value = ((AliasedValue)record[key]).Value;
+            }
+            else
+            {
+                value = record[key];
+            }
+
+            if (value is int) result = (value).ToString();
+            else if (value is decimal) result = (value).ToString();
+            else if (value is Money) result = ((Money)value).Value.ToString();
+            else if (value is EntityReference) result += ((EntityReference)value).Id.ToString();
+            else if (value is OptionSetValue) result = ((OptionSetValue)record[key]).Value.ToString();
+            else if (value is bool) result = ((bool)record[key]).ToString();
+            else if (value is DateTime) result = ((DateTime)record[key]).ToString();
+            else if (value is string) result += (string)value;
+            
+            return result;
+        }
+
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            var target = (Entity)context.InputParameters["Target"];
+
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if (config != null)
+            {
+                string fetchXml = config.FetchXML;
+                int count = 0;
+                try
+                {
+                   
+                    string value = null;
+                    foreach (var attr in target.Attributes)
+                    {
+                        value = getStringValue(target, attr.Key);
+                        fetchXml = fetchXml.Replace("{" + attr.Key + "}", value);
+                    }
+
+                    value = context.InitiatingUserId.ToString();
+                    fetchXml = fetchXml.Replace("{modifiedby}", value);
+                    FetchExpression fe = new FetchExpression(fetchXml);
+                    count = service.RetrieveMultiple(fe).Entities.Count;
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException(ex.Message + fetchXml);
+                }
+
+                //throw new InvalidPluginExecutionException(count.ToString() + config.ErrorWhenEmpty.ToString());
+                if ((count > 0 && !config.ErrorWhenEmpty) ||
+                    (count == 0 && config.ErrorWhenEmpty))
+                {
+                    throw new InvalidPluginExecutionException(config.ErrorMessage);
+                }
+                
+
+            }
+            
+        }
+    }
+}
